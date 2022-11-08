0deb657fae4ffd82f07639c63f8a755beb764086
Rohinireddy <chinnirohinireddy@gmail.com>
2022-11-08 15:23:47 +0530

    updated

--- src/main/java/com/rarjavademo/tables/TablesApplication.java
+++ src/main/java/com/rarjavademo/tables/TablesApplication.java
@@ -8,7 +8,7 @@ public class TablesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TablesApplication.class, args);
-		System.out.println("Project started");
+		System.out.println("Project updated");
 	}
 
 }
