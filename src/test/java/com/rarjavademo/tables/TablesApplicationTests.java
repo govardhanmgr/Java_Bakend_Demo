50cde6f1578a02b476a8385c5b26b17b513834e4
Girish <kolliparagirish61@gmail.com>
2022-11-08 10:44:13 +0530

    first commit

---
+++ src/test/java/com/rarjavademo/tables/TablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rarjavademo.tables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
