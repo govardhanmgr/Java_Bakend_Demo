a4ec97c1ddaa71e1b94a93f95d5cb0d48f2794a8
Govardhan Reddy <morsu.gr000.com>
2022-11-08 14:30:47 +0530

    upated

--- src/main/java/com/rarjavademo/tables/TablesApplication.java
+++ src/main/java/com/rarjavademo/tables/TablesApplication.java
@@ -8,6 +8,7 @@ public class TablesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TablesApplication.class, args);
+		System.out.println("Project started");
 	}
 
 }
