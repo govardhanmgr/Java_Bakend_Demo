11d15ff6eca95fbd02c8c194def54c4e9cc3aaa5
channupraveen <111118593+channupraveen@users.noreply.github.com>
2022-11-10 18:59:05 +0530

    update

--- src/main/java/com/rarjavademo/tables/services/PayrollExpensesServices.java
+++ src/main/java/com/rarjavademo/tables/services/PayrollExpensesServices.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.rarjavademo.tables.dto.PayrollExpensesDto;
+import com.rarjavademo.tables.model.EmpdataModel;
 import com.rarjavademo.tables.model.PayrollExpensesModel;
 import com.rarjavademo.tables.repository.PayrollExpensesRepository;
 
@@ -25,6 +26,27 @@ public class PayrollExpensesServices {
 	        }
 	        return edto;
 	    }
+
+		public void savePayrollexpense(PayrollExpensesDto predto) {
+			PayrollExpensesModel payex = new PayrollExpensesModel();
+			payex.setCreatedby(predto.getCreatedby());
+			payex.setEmployeeid(predto.getEmployeeid());
+			payex.setPayperiodenddate(predto.getPayperiodenddate());
+			payex.setPayperiodenddate(predto.getPayperiodenddate());
+			payex.setPaymentdate(predto.getpaymentdate());
+			payex.setNoofhours(predto.getNoofhours());
+			payex.setPayrate(predto.getPayrate());
+			payex.setGrosspay(predto.getGrosspay());
+			payex.setPayrollexpense(predto.getPayrollexpense());
+			payex.setInsurancebycompany(predto.getInsurancebycompany());
+			payex.setTotalpayroll(predto.getTotalpayroll());
+			payex.setCreatioddate(predto.getCreatioddate());
+			payex.setCreatedby(predto.getCreatedby());
+			payex.setUpdatedby(predto.getUpdatedby());
+			payex.setUpdatedby(predto.getUpdatedby());
+			er.save(payex);
+			
+		}
 	
 
 	
